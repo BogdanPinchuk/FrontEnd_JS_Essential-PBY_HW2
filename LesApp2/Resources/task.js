a65c3482cf31fe7ef63772449f197dc8b882fd89
Bogdan <pinchuk.brus@windowslive.com>
2021-06-21 17:00:32 +0300

    Added part 5

---
+++ LesApp2/Resources/task.js
@@ -0,0 +1,15 @@
+console.log("Дано:");
+
+// input data
+let x = 10,
+    y = 20;
+
+console.log(`${x} == ${y} : ${x == y}`);
+console.log(`${x} === ${y} : ${x === y}`);
+console.log(`typeof(${x}) == typeof(${y}) : ${typeof(x) === typeof(y)}`);
+console.log(`${x} != ${y} : ${x != y}`);
+console.log(`${x} !== ${y} : ${x !== y}`);
+console.log(`${x} < ${y} : ${x < y}`);
+console.log(`${x} <= ${y} : ${x <= y}`);
+console.log(`${x} > ${y} : ${x > y}`);
+console.log(`${x} >= ${y} : ${x >= y}`);
